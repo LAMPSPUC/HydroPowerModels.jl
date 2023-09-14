01c0ff899154eda60864c62beda79b91be2dc4c1
Thiago Novaes <thiagonovaesb@poli.ufrj.br>
2023-09-14 16:06:42 +0200

    done

--- test/IO.jl
+++ test/IO.jl
@@ -15,4 +15,10 @@ using GLPK
         @test params["optimizer"] == optimizer
         @test params["model_constructor_grid"] == DCPPowerModel
     end
+
+    @testset "Validate Json" begin
+        @test isnothing(validate_json_hydro("..\\testcases\\validate_json\\correct_json.jl"))
+        @test !isnothing(validate_json_hydro("..\\testcases\\validate_json\\missing_json.jl"))
+        @test !isnothing(validate_json_hydro("..\\testcases\\validate_json\\type_json.jl"))
+    end
 end
