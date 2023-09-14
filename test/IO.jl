c9d4b81023d3d73538132d6a984a19c8fe08ea07
Thiago Novaes <thiagonovaesb@poli.ufrj.br>
2023-09-14 17:25:46 +0200

    cover

--- test/IO.jl
+++ test/IO.jl
@@ -18,13 +18,23 @@ using GLPK
 
     @testset "Validate Json" begin
         @test isnothing(
-            validate_json_hydro(joinpath(testcases_dir, "validate_json", "correct.json"))
+            validate_json_hydro(joinpath(testcases_dir, "validate_json", "hydro_correct.json"))
         )
         @test !isnothing(
-            validate_json_hydro(joinpath(testcases_dir, "validate_json", "missing.json"))
+            validate_json_hydro(joinpath(testcases_dir, "validate_json", "hydro_missing.json"))
         )
         @test !isnothing(
-            validate_json_hydro(joinpath(testcases_dir, "validate_json", "type.json"))
+            validate_json_hydro(joinpath(testcases_dir, "validate_json", "hydro_type.json"))
+        )
+
+        @test isnothing(
+            validate_json_powermodels(joinpath(testcases_dir, "validate_json", "powermodels_correct.json"))
+        )
+        @test !isnothing(
+            validate_json_powermodels(joinpath(testcases_dir, "validate_json", "powermodels_missing.json"))
+        )
+        @test !isnothing(
+            validate_json_powermodels(joinpath(testcases_dir, "validate_json", "powermodels_type.json"))
         )
     end
 end
